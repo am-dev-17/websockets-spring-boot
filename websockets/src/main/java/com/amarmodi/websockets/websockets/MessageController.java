aea14dc647ab09f93814f740a33b221760e82dd2
Amar Modi <amar.modi2020@gmail.com>
2022-03-23 17:07:25 -0400

    adding the websocket and websockets client for comparision and implementation

---
+++ websockets/src/main/java/com/amarmodi/websockets/websockets/MessageController.java
@@ -0,0 +1,17 @@
+package com.amarmodi.websockets.websockets;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MessageController {
+
+    // Handles messages from /app/chat. (Note the Spring adds the /app prefix for us).
+    @MessageMapping("/chat")
+    // Sends the return value of this method to /topic/messages
+    @SendTo("/topic/messages")
+    public MessageDto getMessages(MessageDto messageDto) {
+        return messageDto;
+    }
+}
---
+++ websockets/src/main/java/com/amarmodi/websockets/websockets/MessageDto.java
@@ -0,0 +1,19 @@
+package com.amarmodi.websockets.websockets;
+
+public class MessageDto {
+
+    private String message;
+    
+    public String getMessage() {
+    
+        return message;
+    
+    }
+    
+    public void setMessage(String message) {
+    
+        this.message = message;
+    
+    }
+
+}
